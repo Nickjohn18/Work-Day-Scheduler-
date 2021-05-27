bb13377c3ffc69dc7911ae58ddb6754b7cb49f63
Nickjohn18 <81334326+Nickjohn18@users.noreply.github.com>
2021-05-27 18:45:14 -0400

    added inputs for reminder

--- Develop/scheduler.js
+++ Develop/scheduler.js
@@ -12,4 +12,35 @@ var currentTime = moment();
 currentTime = currentTime.startOf("hour");
 }
 
+dayTime.forEach(function(time) {
+    console.log(time)
 
+    var dayHours = [];
+    dayHours.push(moment().hour(time).minutes("00").format('hh:mm A'));
+
+    var hourDiv = $("<div>");
+    hourDiv.addClass("input-group input-group-lg pb-1 hour");
+    
+    var hourSpan = $("<span>");
+    hourSpan.text(dayTime.length)
+    hourSpan.addClass("input-group-text time-block block");
+    hourDiv.append(hourSpan); 
+    
+    var hourInput = $("<input>").attr('type', 'text');
+    hourInput.addClass("form-control form");
+    hourInput.data("Hour");
+    hourDiv.append(hourInput);
+
+
+    var hourSubmit = $("<button>").attr("type", 'click');
+    hourSubmit.addClass("btn saveBtn");
+    hourDiv.append(hourSubmit);
+
+
+    var hourIcon = $("<i class ='far fa-save fa-lg'></i>")
+    hourSubmit.append(hourIcon)
+
+
+    $(".container").append(hourDiv);
+    
+})
