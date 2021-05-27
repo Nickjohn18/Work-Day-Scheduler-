4418d8f9ae60d35e8c74d61d224b788dfd9c81ae
Nickjohn18 <81334326+Nickjohn18@users.noreply.github.com>
2021-05-27 18:44:24 -0400

    initial js and added current time

---
+++ Develop/scheduler.js
@@ -0,0 +1,15 @@
+var dayTime = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+var hours = document.getElementById("hours")
+
+
+
+moment(Date)
+ $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+
+
+function getTime(){
+var currentTime = moment();
+currentTime = currentTime.startOf("hour");
+}
+
+
